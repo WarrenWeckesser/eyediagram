04c72c8123a2841be3487fdf126a803538f74e3f
Warren Weckesser <warren.weckesser@gmail.com>
2015-03-20 16:22:24 -0400

    MAINT: update version to 0.0.2-dev

--- eyediagram/__init__.py
+++ eyediagram/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2015, Warren Weckesser.  All rights reserved.
 # This software is licensed according to the "BSD 2-clause" license.
 
-__version__ = "0.0.1"
+__version__ = "0.0.2-dev"
 
 import core
 import bok

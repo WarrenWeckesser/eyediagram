a628845c5878d70b8fa8433e8098adcd3a923d4c
Warren Weckesser <warren.weckesser@gmail.com>
2015-03-21 08:52:39 -0400

    REL: Update version to 0.0.3-dev

--- eyediagram/__init__.py
+++ eyediagram/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2015, Warren Weckesser.  All rights reserved.
 # This software is licensed according to the "BSD 2-clause" license.
 
-__version__ = "0.0.2"
+__version__ = "0.0.3-dev"
